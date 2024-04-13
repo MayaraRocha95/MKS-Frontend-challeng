282e508e7d45c75c40f38ad216233bd0cffd1c61
MayaraRocha95 <mayaraaraujorocha@hotmail.com>
2024-04-13 14:29:09 -0300

    Refactor page component and add new components in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,39 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client" 
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Store from "./components/Store";
+import styled from 'styled-components';
+import { useState } from 'react';
+import Sidebar from "./components/Sidebar";
+
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  photo: string;
+  quantity: number;
+}
+
+
+const Container = styled.div`
+display: flex;
+justify-content: space-between;
+flex-direction: column;
+margin: 0;
+`;
+
+
 
 export default function Home() {
+  const [cart , setCart] = useState([] as Product[]);
+  const [showSidebar, setShowSidebar] = useState(false);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Container>
+      <Header cart={cart} setCart={setCart}  showSidebar={showSidebar} setShowSidebar={setShowSidebar}/>
+      <Store cart={cart} setCart={setCart} />
+      <Footer />
+      {showSidebar && <Sidebar cart={cart} setCart={setCart}   showSidebar={showSidebar} setShowSidebar={setShowSidebar} />}
+    </Container>
   );
 }
